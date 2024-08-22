9bd7e1b3846ca1aedd8de5e6e3ac78fe6f338da6
HanSanghyun <56988779+Hansanghyun-github@users.noreply.github.com>
2024-08-22 16:19:54 +0900

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,11 @@
 echo "test sh"
 
-if [ ${var} -eq 12 ]; then echo "equal"; else "not equal"; fi
+if ! [[ ${var} =~ ^[0-9]+$ ]]; then
+    echo "var is not a number"
+elif [ ${var} -eq 12 ]; then
+    echo "equal"
+else
+    echo "not equal"
+fi
 
 echo "test finished"
